4d304f5a5270c4b3e3e9ab8e5f3726ef2ac04816
lpien <lpien@u.rochester.edu>
2015-01-31 17:04:12 -0500

    Update printConsumer.java

--- printConsumer.java
+++ printConsumer.java
@@ -1,10 +1,8 @@
-/**
- * 
- * @author Lauren
- * lpien@u.rochester.edu
- *
- * @param <AnyType>
- */
+/*
+If the total word count is greater than 40 words, the output will only print the 
+first and last 20 words with ellipses in between.
+*/
+
 public class printConsumer <AnyType> implements Consumer<AnyType>{
 	public int total;
 	public int count;
@@ -17,15 +15,15 @@ public class printConsumer <AnyType> implements Consumer<AnyType>{
 	public void accept(AnyType actor){
 		count++;
 		if(total>40){
-		if(count <= 20){
-			System.out.println(actor.toString());
-		}
-		if(count == 21){
-			System.out.println("...");
-		}
-		if(count >= total - 20){
-			System.out.println(actor.toString());
-		}
+			if(count <= 20){
+				System.out.println(actor.toString());
+			}
+			if(count == 21){
+				System.out.println("...");
+			}
+			if(count >= total - 20){
+				System.out.println(actor.toString());
+			}
 		}
 		else{
 			System.out.println(actor.toString());
